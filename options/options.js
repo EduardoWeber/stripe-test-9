c9641edd16dbbffb40f05a9b1a25c9ae5e5b2001
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2022-07-14 18:06:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "stripe_test_9",
+  url: "https://stripe_test_9.botics.co",
+  api: "https://stripe_test_9.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
